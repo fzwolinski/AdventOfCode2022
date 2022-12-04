ac5dd27e63ced001ca2597a5d87130420350b6f9
fzwolinski <Giovacho@tuta.io>
2022-12-04 22:55:27 +0100

    --- Day 2: Rock Paper Scissors | Refactor ---

--- day2/day2.cpp
+++ day2/day2.cpp
@@ -1,26 +1,40 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <sstream>
 #include <string>
 #include <tuple>
 #include <vector>
 
-using Shape = std::string;
+enum class Shape {
+  rock = 1,
+  paper = 2,
+  scissors = 3
+};
+
+enum class Result {
+  loss = 0,
+  draw = 3,
+  win = 6
+};
+
+using Short = std::string;
+
+std::map<Short, Shape> shapeMap {
+  {"X", Shape::rock},
+  {"Y", Shape::paper},
+  {"Z", Shape::scissors},
+  {"A", Shape::rock},
+  {"B", Shape::paper},
+  {"C", Shape::scissors}
+};
+
 //                         oponent  me
 using TurnPair = std::pair<Shape,   Shape>;
 
-// Opponent
-const Shape oRock = Shape("A");
-const Shape oPaper = Shape("B");
-const Shape oScissors = Shape("C");
-
-// Me
-const Shape mRock = Shape("X");
-const Shape mPaper = Shape("Y");
-const Shape mScissors = Shape("Z");
-
 std::vector<TurnPair> strategyGuide(std::ifstream& file);
+Shape getShapeFromShort(const Short &_short);
 unsigned int getPointsForShape(const Shape& s);
 unsigned int getPointsForRoundOutcome(const TurnPair &round);
 std::tuple<unsigned int, unsigned int> getPointsForTurn(const TurnPair &round);
@@ -55,9 +69,10 @@ std::vector<TurnPair> strategyGuide(std::ifstream& file) {
   std::string fileLine;
   while(std::getline(file, fileLine)) {
     std::istringstream iss(fileLine);
-    std::string myChoice, opponentChoice;
+    Short myChoice, opponentChoice;
     if((iss >> myChoice >> opponentChoice)) {
-      strategyGuide.push_back({myChoice, opponentChoice});
+      strategyGuide.push_back({getShapeFromShort(myChoice), 
+                               getShapeFromShort(opponentChoice)});
     } else {
       break;
     }
@@ -65,54 +80,34 @@ std::vector<TurnPair> strategyGuide(std::ifstream& file) {
   return strategyGuide;
 }
 
+Shape getShapeFromShort(const Short &_short) {
+  return shapeMap.at(_short);
+}
+
 unsigned int getPointsForShape(const Shape& s) {
-  /*
-   * X - Rock     - 1 pt
-   * Y - Paper    - 2 pt
-   * Z - Scissors - 3 pt
-   *
-   */
-
-  unsigned int retVal{0u};
-  
-  if (s == mRock) {
-    retVal = 1u;
-  } else if (s == mPaper) {
-    retVal = 2u;
-  } else if (s == mScissors) {
-    retVal = 3u;
-  }
-
-  return retVal;
+  return static_cast<unsigned int>(s);
 }
 
 unsigned int getPointsForRoundOutcome(const TurnPair &round) {
   const Shape &opponent = round.first;
   const Shape &me = round.second;
 
-  /*
-   * lose 0 pt
-   * draw 3 pt
-   * win  6 pt
-   *
-   */
-
   // Draw scenarios
-  if ((me == mRock && opponent == oRock) ||
-      (me == mPaper && opponent == oPaper) ||
-      (me == mScissors && opponent == oScissors)) {
-    return 3u;
+  if ((me == Shape::rock && opponent == Shape::rock) ||
+      (me == Shape::paper && opponent == Shape::paper) ||
+      (me == Shape::scissors && opponent == Shape::scissors)) {
+    return static_cast<unsigned int>(Result::draw);
   }
 
   // Win scenarios
-  if ((me == mRock && opponent == oScissors) ||
-      (me == mScissors && opponent == oPaper) ||
-      (me == mPaper && opponent == oRock)) {
-    return 6u;
+  if ((me == Shape::rock && opponent == Shape::scissors) ||
+      (me == Shape::scissors && opponent == Shape::paper) ||
+      (me == Shape::paper && opponent == Shape::rock)) {
+    return static_cast<unsigned int>(Result::win);
   }
 
-  // The rest are lose scenarios
-  return 0u;
+  // The rest are loss scenarios
+  return static_cast<unsigned int>(Result::loss);  
 }
 
 std::tuple<unsigned int, unsigned int> getPointsForTurn(const TurnPair &round) {
@@ -134,47 +129,48 @@ std::tuple<unsigned int, unsigned int> getPointsForTurn(const TurnPair &round) {
 
 Shape iShouldPlay(const TurnPair &round) {
   /*
-   * X - I need to lose
-   * Y - I need to draw
-   * Z - I need to win
+   * X - Rock     - I need to loss
+   * Y - Paper    - I need to draw
+   * Z - Scissors - I need to win
    *
    */
+
   const Shape &oponentChoice = round.first;
   const Shape &desiredResult = round.second;
 
-  if (desiredResult == "X") {
-    if (oponentChoice == oRock) {
-      return mScissors;
+  if (desiredResult == Shape::rock) {
+    if (oponentChoice == Shape::rock) {
+      return Shape::scissors;
     }
-    if (oponentChoice == oScissors) {
-      return mPaper;
+    if (oponentChoice == Shape::scissors) {
+      return Shape::paper;
     }
-    if (oponentChoice == oPaper) {
-      return mRock;
-    }
-  }
-
-  if (desiredResult == "Y") {
-    if (oponentChoice == oRock) {
-      return mRock;
-    }
-    if (oponentChoice == oScissors) {
-      return mScissors;
-    }
-    if (oponentChoice == oPaper) {
-      return mPaper;
+    if (oponentChoice == Shape::paper) {
+      return Shape::rock;
     }
   }
 
-  if (desiredResult == "Z") {
-    if (oponentChoice == oRock) {
-      return mPaper;
+  if (desiredResult == Shape::paper) {
+    if (oponentChoice == Shape::rock) {
+      return Shape::rock;
     }
-    if (oponentChoice == oScissors) {
-      return mRock;
+    if (oponentChoice == Shape::scissors) {
+      return Shape::scissors;
     }
-    if (oponentChoice == oPaper) {
-      return mScissors;
+    if (oponentChoice == Shape::paper) {
+      return Shape::paper;
+    }
+  }
+
+  if (desiredResult == Shape::scissors) {
+    if (oponentChoice == Shape::rock) {
+      return Shape::paper;
+    }
+    if (oponentChoice == Shape::scissors) {
+      return Shape::rock;
+    }
+    if (oponentChoice == Shape::paper) {
+      return Shape::scissors;
     }
   }
 }
