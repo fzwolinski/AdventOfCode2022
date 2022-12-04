d1618089d62bb906f42c41bf7a9a45523c1f3491
fzwolinski <Giovacho@tuta.io>
2022-12-04 12:19:58 +0100

    --- Day 2: Rock Paper Scissors | Part 2 ---

--- day2/day2.cpp
+++ day2/day2.cpp
@@ -3,8 +3,8 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <tuple>
 #include <vector>
-#include <utility>
 
 using Shape = std::string;
 //                         oponent  me
@@ -23,7 +23,8 @@ const Shape mScissors = Shape("Z");
 std::vector<TurnPair> strategyGuide(std::ifstream& file);
 unsigned int getPointsForShape(const Shape& s);
 unsigned int getPointsForRoundOutcome(const TurnPair &round);
-unsigned int getPointsForTurn(const TurnPair &round);
+std::tuple<unsigned int, unsigned int> getPointsForTurn(const TurnPair &round);
+Shape iShouldPlay(const TurnPair &round);
 
 int main() {
   std::ifstream file("day2_input.txt");
@@ -34,12 +35,17 @@ int main() {
 
   auto strategy = strategyGuide(file);
   
-  unsigned int myScore{0u};
+  unsigned int scoreStrategyGuide{0u};
+  unsigned int scoreDesiredOutcome{0u};
+  
   for (TurnPair turn : strategy) {
-    myScore += getPointsForTurn(turn);
+    auto [temp1, temp2] = getPointsForTurn(turn);
+    scoreStrategyGuide += temp1;
+    scoreDesiredOutcome += temp2;
   }
   
-  std::cout << "My score: " << myScore << "\n";
+  std::cout << "Total score: " << scoreStrategyGuide << "\n";
+  std::cout << "Total score with desired outcome: " << scoreDesiredOutcome << "\n";
 
   return 0;
 }
@@ -109,14 +115,67 @@ unsigned int getPointsForRoundOutcome(const TurnPair &round) {
   return 0u;
 }
 
-unsigned int getPointsForTurn(const TurnPair &round) {
+std::tuple<unsigned int, unsigned int> getPointsForTurn(const TurnPair &round) {
   unsigned int points{0u};
+  unsigned int pointsDesiredReturn{0u};
+  const Shape shapeIShouldPlay = iShouldPlay(round);
+  const TurnPair desiredTurn{round.first, shapeIShouldPlay};
+
   // Points for the shape I selected
   points += getPointsForShape(round.second);
+  pointsDesiredReturn += getPointsForShape(shapeIShouldPlay);
   
   // Points for the outcome of the round
   points += getPointsForRoundOutcome(round);
+  pointsDesiredReturn += getPointsForRoundOutcome(desiredTurn);
 
-  return points;
+  return {points, pointsDesiredReturn};
+}
+
+Shape iShouldPlay(const TurnPair &round) {
+  /*
+   * X - I need to lose
+   * Y - I need to draw
+   * Z - I need to win
+   *
+   */
+  const Shape &oponentChoice = round.first;
+  const Shape &desiredResult = round.second;
+
+  if (desiredResult == "X") {
+    if (oponentChoice == oRock) {
+      return mScissors;
+    }
+    if (oponentChoice == oScissors) {
+      return mPaper;
+    }
+    if (oponentChoice == oPaper) {
+      return mRock;
+    }
+  }
+
+  if (desiredResult == "Y") {
+    if (oponentChoice == oRock) {
+      return mRock;
+    }
+    if (oponentChoice == oScissors) {
+      return mScissors;
+    }
+    if (oponentChoice == oPaper) {
+      return mPaper;
+    }
+  }
+
+  if (desiredResult == "Z") {
+    if (oponentChoice == oRock) {
+      return mPaper;
+    }
+    if (oponentChoice == oScissors) {
+      return mRock;
+    }
+    if (oponentChoice == oPaper) {
+      return mScissors;
+    }
+  }
 }
 
